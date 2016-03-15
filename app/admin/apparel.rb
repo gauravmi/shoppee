15e3c60e2d18b4feb9df328ed082fc0d861da2ce
KavithaR <kavithar@thoughtworks.com>
2016-03-15 18:43:26 +0530

    Removed count from apparel model

--- app/admin/apparel.rb
+++ app/admin/apparel.rb
@@ -2,14 +2,12 @@ ActiveAdmin.register Apparel  do
   index do
     column :size
     column :apparel_type
-    column :count
 	end
 
   form do |f|
     f.inputs 'apparels' do
       f.input :size, as: :select, collection: ['XS','S','M','L']
       f.input :apparel_type, as: :select, collection: ['Shirt', 'Skirt', 'Trousers']
-      f.input :count
     end
     f.button :Submit
 	end
--- db/migrate/20160315130034_add_column_count.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnCount < ActiveRecord::Migration
-  def up
-    add_column :apparels, :count, :integer
-  end
-
-  def down
-    remove_column :apparels, :count
-  end
-end
